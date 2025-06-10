d02cdda3ea34ccef4dda3f8f617f1c1742b609d0
Dan <danflorezmartinez@gmail.com>
2025-06-10 07:31:18 -0500

    Creacion del repositorio de Roles, con algunas consultas escenciales

---
+++ src/app/rol/repository/sql_roles.ts
@@ -0,0 +1,30 @@
+export const SQL_ROL = {
+    FIND_ALL: `SELECT id,
+        nombre
+        FROM roles`,
+
+    FIND_ID: `SELECT id,
+        nombre
+    FROM roles
+    WHERE id = $1`,
+
+    FIND_NAME: `SELECT id,
+        nombre
+    FROM roles
+    WHERE nombre = $1`,
+
+    HOW_MANY: `SELECT count(nombre) as cantidad
+    FROM roles
+    WHERE nombre = $1`,
+
+    ADD: `INSERT INTO roles (nombre)
+    VALUES ($1)
+    RETURNING id, nombre`,
+
+    UPDATE: `UPDATE roles
+    SET nombre
+    WHERE id = $1`,
+
+    DELETE: `DELETE FROM roles
+    WHERE id = $1`
+}
