55e055b0127dcd754bf96cbab5c05db88e9db3dc
Dan <danflorezmartinez@gmail.com>
2025-06-19 15:24:48 -0500

    Implementación de la clase TipoEtiqueta y consultas SQL para gestionar tipos de etiquetas, incluyendo operaciones de búsqueda, adición, actualización y eliminación.

---
+++ src/app/tipo_etiqueta/model/TipoEtiqueta.ts
@@ -0,0 +1,27 @@
+class TipoEtiqueta {
+    private _id: number;
+    private _nombre: string;
+
+    constructor(id: number, nombre: string) {
+        this._id = id;
+        this._nombre = nombre;
+    }
+
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
+
+    get nombre(): string {
+        return this._nombre;
+    }
+
+    set nombre(value: string) {
+        this._nombre = value;
+    }
+}
+
+export default TipoEtiqueta;
